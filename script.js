ed3f6930f2b85a5e5002534627f07a02683e96ae
kboduck <39456236+kboduck@users.noreply.github.com>
2022-12-22 10:56:10 +0200

    add images block7

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', e => {
     let navbar = document.getElementById('navbar').classList;
-    let active_class = "header_scrolled";
+    let active_class = "menu_scrolled";
     if(pageYOffset > 120){
         navbar.add(active_class)
 }
